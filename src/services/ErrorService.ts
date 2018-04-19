e46fdec138c0a833f525a990c6ed24803467fd87
abuzaforfagun <mdabuzaforfagun@gmail.com>
2018-04-19 10:07:46 +0600

    refactor

--- src/services/ErrorService.ts
+++ src/services/ErrorService.ts
@@ -19,40 +19,11 @@ export class ErrorService {
         this.errors = [];
     }
 
-    getErrorCount() {
-        this.totalErrors = this.errors.length;
-        return this.totalErrors;
-    }
 
     getErrors(offset: number): Promise<any> {
         let limit = 50;
         const url ="error-reporting/logs/?limit=" + limit + "&offset=" + offset;
         return this.httpHelper.getData(url, "results", false);
-        // return new Promise(resolve => {
-        //     this.tokenService.getTokens().then((header: any) => {
-        //         this.http.get(this.constantService.getAPIRoot() + "error-reporting/logs/?limit=" + limit + "&offset=" + offset, {
-        //             headers: {
-        //                 'Authorization': header
-        //             },
-
-        //         }).subscribe(data => {
-        //             var result = JSON.parse(JSON.stringify(data)).results;
-        //             if (this.constantService.isResultEmpty(result)) {
-        //                 resolve(null);
-        //             }
-        //             this.errors.push(result);
-        //             resolve(result);
-        //         }, error => {
-        //             resolve(this.errors)
-        //         });
-        //     })
-        // })
     }
 
-    getError(id: number): Promise<any> {
-        return new Promise((resolve, reject) => {
-            resolve(this.errors.find(e => e.id == id));
-
-        });
-    }
 }
