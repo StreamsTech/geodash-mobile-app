c82c57621e73ab94742c6e4a103746e15bc65c72
abuzaforfagun <mdabuzaforfagun@gmail.com>
2018-04-19 09:51:08 +0600

    Implement get http request in httpservice

--- src/services/HttpHelperService.ts
+++ src/services/HttpHelperService.ts
@@ -11,37 +11,55 @@ export class HttpHelperService {
     constructor(
         private tokenService: TokenAuthenticationService,
         private constantService: ConstantService,
-        private localStorageService:Storage,
         private http: HttpClient) {
 
     }
 
-    createAuthorizationHeader() {
-        const headers = new HttpHeaders({
-          'Authorization': 'Bearer ' + this.localStorageService.getItem('token')
-        });
-        return headers;
-    }
+    getData(url, decorateDate = false) {
+        let items = [];
+        return new Promise(resolve => {
+            this.tokenService.getTokens().then((header: any) => {
+                this.http.get(this.constantService.getAPIRoot() + url, {
+                    headers: {
+                        'Authorization': header
+                    },
+                })
+                    .subscribe(data => {
+                        var result = JSON.parse(JSON.stringify(data)).objects;
+                        let date = new Date().getTime();
+                        if (decorateDate) {
+                            this.decorateDateObject(result, date);
+                        }
+                        if (this.constantService.isResultEmpty(result)) {
+                            resolve(items);
+                            return items;
+                        }
+                        items = result;
+                        resolve(items);
+                        return items;
+                    }, error => {
+                        resolve(items);
+                        return items;
+                    })
 
-    getData(url) {
-        this.http
-            .get(this.constantService.getAPIRoot() + url, {
-                headers: this.createAuthorizationHeader()
-            }).map(res => res);
-
-    }
-    public getAuthenticationHeader(): Promise<any> {
-        return new Promise((resolve) => {
-            // this.tokenService.getTokens().then((data: any) => {
-            //     let header = "";
-            //     header = 'Bearer ' + data;
-
-            //     resolve(header);
-            //     console.log("header");
-            //     console.log(JSON.stringify(header));
-            //     return header;
-            // })
+            })
         })
     }
 
+    private decorateDateObject(result: any, date: number) {
+        for (let item of result) {
+            let itemDate = new Date(item.date).getTime();
+            let timeDiffrence: number = date - itemDate;
+            let hours: number = timeDiffrence / (1000 * 60 * 60);
+            var days: any = parseInt((hours / 24).toString());
+            hours = parseInt((hours % 24).toString());
+            if (days > 0) {
+                item.date = days + " Days";
+            }
+            if (hours > 0) {
+                item.date += " and " + hours + " Hours";
+            }
+            item.date += " ago";
+        }
+    }
 }
